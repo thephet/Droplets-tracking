4de358484141b4d700186c26c68dca8cce4ce12c
Juan Manuel Parrilla Gutierrez <dogmadefe@hotmail.com>
2012-02-29 14:52:57 +0100

    Initial py files to add

---
+++ ParticleFilter.py
@@ -0,0 +1,137 @@
+from random import random, uniform
+from math import sqrt, cos, sin
+
+
+class Condensation:
+
+	def __init__(self, numParticles, iniX, iniY, maxX, maxY):
+		''' iniX iniY maxX and maxY refer to the area where initially and randomly
+		we will place the particles'''
+
+		self.numTargets = 0
+		self.numParticles = numParticles
+		self.particles = []
+		self.estimation = []
+
+		for i in range(numParticles):
+			x = uniform(iniX, maxX) #generate a ran num between iniX and maxX
+			y = uniform(iniY, maxY)
+			self.particles.append( Particle(x,y) )
+
+
+	def propagate(self, propData):
+
+		for particle in self.particles:
+			target = particle.myTarget()
+			target = propData[target]
+			particle.propagate(target)
+
+	def updateWeights(self, propData):
+
+		for particle in self.particles:
+			particle.updateWeights(propData)
+
+		sum_weights = [0] * self.numTargets
+		for particle in self.particles:
+			for j in range(self.numTargets):
+				sum_weights[j] += particle.weights[j]
+
+		for particle in self.particles:
+			particle.normWeight(sum_weights)
+
+	def reSampling(self):
+
+		temp_particles = []
+
+		for j in range(self.numTargets):
+			cumulative = [0] * (self.numParticles+1)
+			
+			for i in range(self.numParticles):
+				cumulative[i+1] = cumulative[i] + self.particles[i].weights[j]
+
+			for i in range(self.numParticles/self.numTargets):
+				ranNum = random()
+				k = 1
+				while ranNum > cumulative[k]: 
+					k+=1
+				temp_particles.append( self.particles[k-1].copy() )
+
+		while len(temp_particles) < self.numParticles:
+			randParticle = int( uniform( 0, self.numParticles ) )
+			temp_particles.append( self.particles[randParticle].copy() )
+
+		self.particles = temp_particles
+
+
+	def estimateState(self):
+
+		self.estimation = [dict(x=0, y=0) for i in range(self.numTargets)]
+		x = [0] * self.numTargets
+		y = [0] * self.numTargets
+		counter = [0] * self.numTargets
+
+		for particle in self.particles:
+			target = particle.myTarget()
+			x[target] += particle.weights[target] * particle.x
+			y[target] += particle.weights[target] * particle.y
+			counter[target] += particle.weights[target]
+
+		for i in range(self.numTargets):
+			if counter[i] > 0:
+				self.estimation[i]['x'] = x[i] / counter[i]
+				self.estimation[i]['y'] = y[i] / counter[i]
+
+
+	def addTarget(self):
+		self.numTargets+=1
+		for particle in self.particles:
+			particle.weights.append(0)
+
+
+
+class Particle:
+
+	def __init__(self, inX, inY):
+		self.x = inX
+		self.y = inY
+		self.weights = []
+
+	def copy(self):
+		newParticle = Particle(self.x, self.y)
+		newParticle.weights = self.weights[:]
+		return newParticle
+
+	def propagate(self, propData):
+
+		ranDir = uniform(-0.5, 0.5)
+		ranDis = uniform(0, 2)
+
+		self.x += ( (propData['speed'] + propData['acceleration']/2) 
+				* cos(propData['direction']+propData['changeDirection']+ranDir) * ranDis )
+		self.y += ( (propData['speed'] + propData['acceleration']/2) 
+				* sin(propData['direction']+propData['changeDirection']+ranDir) * ranDis )
+
+
+	def updateWeights(self, propData):
+
+		for i in range(len(self.weights)):
+			pData = propData[i]
+			self.weights[i] = 1 / sqrt( (pData['lastPoint']['x']-self.x)**2 
+								+ (pData['lastPoint']['y']-self.y)**2 )
+
+	def normWeight(self, sums):
+
+		for i in range(len(self.weights)):
+			self.weights[i] = self.weights[i] / sums[i]
+
+	def myTarget(self):
+
+		maxWeight = 0
+		maxVal = self.weights[0]
+
+		for i in range(1,len(self.weights)):
+			if self.weights[i] > maxVal:
+				maxWeight = i
+				maxVal = self.weights[i]
+
+		return maxWeight
---
+++ som.py
@@ -0,0 +1,113 @@
+from __future__ import division
+from random import random,shuffle
+from math import exp,log, sqrt
+from sys import maxint
+import numpy as np
+from time import time
+import cv
+import pickle
+
+# 80s
+
+
+class Som:
+
+	def __init__(self, cellsSide, numIts, dimensions, learningRate):
+
+		self.numIts = numIts
+		self.cellsSide = cellsSide
+		self.learningRate = learningRate
+
+		# create all the nodes
+		self.nodes = np.array([ [[ random() for i in range(dimensions) ] for i in range(cellsSide)] for j in range(cellsSide) ])
+
+		# this is the topological 'radius' of the feature map
+		self.mapRadius = cellsSide / 2
+
+		# used in the calculation of the neighbourhood width of m_dInfluence
+		self.timeConstant = numIts / log(self.mapRadius)
+
+	def FindBestMatchingNode(self, data):
+		'''this function presents an input vector to each node in the network
+	  		and calculates the Euclidean distance between the vectors for each
+  			node. It returns a pointer to the best performer'''
+
+  		#this is an array operation, kind of like matlab
+  		position = ((self.nodes-data)**2).sum(axis=2).argmin()
+		i = int(position / len(self.nodes))
+		j = int(position % len(self.nodes))
+
+  		return (i,j)
+
+	def train(self, training_data):
+
+		learningRate = self.learningRate
+
+		for it in xrange(self.numIts):
+			print it
+			# shuffle the training data to have a different order each iteration
+			np.random.shuffle(training_data)
+
+			for data in training_data:
+
+				# present the vector to each node and determine BMU
+				winningNode = self.FindBestMatchingNode(data)
+
+				# calculate the width of the neighbourhood for this timestep
+				neighbourhoodRadius = self.mapRadius*exp(-(it+1)/self.timeConstant)
+				widthSq = neighbourhoodRadius**2
+
+
+				left = winningNode[0] - round(neighbourhoodRadius)
+				if left < 0: left = 0
+				right = winningNode[0] + round(neighbourhoodRadius)
+				if right > len(self.nodes): right = len(self.nodes)
+				up = winningNode[1] - round(neighbourhoodRadius)
+				if up < 0: up = 0
+				down = winningNode[1] + round(neighbourhoodRadius)
+				if down > len(self.nodes): down = len(self.nodes)
+
+				subnodes = self.nodes[left:right, up:down]
+				centeri = (right-left)/2
+				centerj = (down-up)/2
+
+				distToNodeSq = np.array([[ [exp(-((centeri - i)**2 + (centerj-j)**2)/(2*widthSq))] 
+					for i in xrange(int(down-up))] 
+					for j in xrange(int(right-left))])
+				subnodes += learningRate*distToNodeSq*(data-subnodes)
+
+			learningRate = self.learningRate * exp( -(it+1) / self.numIts)
+
+
+if __name__ == "__main__":
+
+	sideSize = 50
+	iterations = 50
+	dimensions = 3
+	learningRate = 0.05
+
+	time_start = time()
+
+	som = Som(sideSize, iterations, dimensions, learningRate)
+
+	training_data = [ [1,0,0], [0,1,0], [0,0.5,0.25], [0,0,1], [0,0,0.5], [1,1,0.2], [1,0.4,0.25], [1,0,1]]
+	f = open('trackNorm.txt', 'r')
+	x = pickle.load(f)
+	som.train(np.array(x))
+
+	results = cv.CreateImage( (500, 500), 8, 3)
+	sideSquare = 500/sideSize
+	for i in xrange(sideSize):
+		for j in xrange(sideSize):
+			cv.Rectangle(results, (int(i*sideSquare), int(j*sideSquare)), (int(i*sideSquare+sideSquare), int(j*sideSquare+sideSquare)), 
+				(som.nodes[i,j,0]*255, 
+				som.nodes[i,j,1]*255, 
+				0), 
+				cv.CV_FILLED)
+	
+	cv.NamedWindow('results', cv.CV_WINDOW_AUTOSIZE)
+	cv.ShowImage('results', results)
+	time_end = time()
+	print time_end - time_start
+	key = cv.WaitKey()
+
